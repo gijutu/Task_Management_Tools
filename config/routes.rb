f519ed62ab1e34cf7a270305185dc8fd034e7c61
gijutu <31819030+gijutu@users.noreply.github.com>
2018-09-10 12:59:26 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'tasks#index'
+  root to:'tasks#index'
+  get 'taks/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
 end
